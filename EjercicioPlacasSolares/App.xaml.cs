6831f00157112d1f3f5a9dbbac3d72deed3aead3
34611 <34611@AJARE>
2023-11-06 19:51:00 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioPlacasSolares/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EjercicioPlacasSolares
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EjercicioPlacasSolares/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EjercicioPlacasSolares
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
